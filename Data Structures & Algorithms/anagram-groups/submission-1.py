2bd341790c1a184c5ee90fa9689c12cd63e65a0f
Adam Le <adamle1025@gmail.com>
2026-08-27 18:16:02 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    # note: a better solution wouldn't use indexes, it would just create a
+    # list of strings and then combine the lists at the end without needing
+    # to change from a list of indexes to their connected strings
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # sort each word, any anagrams will give the same sort
+        # create a dictionary: sorted word -> list of indexes that match
+        anagram_dict = {}
+        index = 0
+        # create the dictionary
+        for s in strs:
+            sorted_word = ''.join(sorted(s))
+            if sorted_word not in anagram_dict.keys():
+                index_list = [index]
+                anagram_dict[sorted_word] = index_list
+            else:
+                index_list = anagram_dict.get(sorted_word)
+                index_list.append(index)
+            index += 1
+        # create the sublists using the indexes
+        sublists = []
+        for key in anagram_dict.keys():
+            indexes = anagram_dict.get(key)
+            sublist = []
+            for i in indexes:
+                sublist.append(strs[i])
+            sublists.append(sublist)
+        return sublists
+        
+
+
+        
